548a4ec8717b2e7669659520c67e8663efb06a8d
sonTung Nguyen <nguyensontungtdn1901@gmail.com>
2024-08-08 18:22:28 +0700

    add: implement dropdown menu with user avatar and username

--- src/app/models/Course.js
+++ src/app/models/Course.js
@@ -1,4 +1,6 @@
 const mongoose = require("mongoose");
+const slug = require("mongoose-slug-updater");
+mongoose.plugin(slug);
 
 const Schema = mongoose.Schema;
 const ObjectId = Schema.ObjectId;
@@ -8,7 +10,7 @@ const Course = new Schema(
     name: { type: String, maxlength: 255, required: true },
     description: { type: String, maxlength: 255 },
     image: { type: String, maxlength: 255 },
-    slug: { type: String, maxlength: 255 },
+    slug: { type: String, slug: "name", unique: true },
     videoId: { type: String, maxlength: 255, required: true },
   },
   { timestamps: true },
