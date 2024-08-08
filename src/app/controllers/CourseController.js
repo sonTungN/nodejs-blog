2fdb0ee27f4cf2f308411f5399e02aa73cc68a95
sonTung Nguyen <nguyensontungtdn1901@gmail.com>
2024-08-08 15:53:18 +0700

    add: implement `courseController` to render course's details and handle `/courses/:slug` routes

---
+++ src/app/controllers/CourseController.js
@@ -0,0 +1,15 @@
+const Course = require("../models/Course");
+const { multipleMongooseToObject } = require("../../utils/mongoose");
+
+class CourseController {
+  // [GET] /courses/:slug
+  show(req, res, next) {
+    Course.findOne({ slug: req.params.slug })
+      .then((course) => {
+        res.json(course);
+      })
+      .catch(next);
+  }
+}
+
+module.exports = new CourseController();
